87f86b999afc874093a112768cea247ee54b9d83
Bruno Guerra <gguerrabruno@outlook.com>
2021-02-16 22:30:00 -0300

    Loading animation

--- lib/user.dart
+++ lib/user.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
 
-class UserPage extends StatefulWidget{
+class UserPage extends StatefulWidget {
   UserPage({Key key, this.title}) : super(key: key);
 
   final String title;
@@ -15,6 +16,7 @@ class UserPage extends StatefulWidget{
 class _UserPageState extends State<UserPage> {
   String username = "";
   GithubData githubData;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,16 +26,20 @@ class _UserPageState extends State<UserPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              githubData == null ? 'Find a user' : 'Bio: ${githubData.bio}'
-            ),
+            Text(githubData == null || githubData.bio == null ? 'Find a user' : 'Bio: ${githubData.bio}'),
             Container(
               width: 180,
               child: TextField(
                 onChanged: (value) => setState(() => username = value),
               ),
             ),
-            ElevatedButton(onPressed: getUserDataFromGithub, child: Text('Find'))
+            isLoading
+                ? SpinKitRotatingCircle(
+                    color: Colors.red,
+                    size: 50.0,
+                  )
+                : ElevatedButton(
+                    onPressed: getUserDataFromGithub, child: Text('Find'))
           ],
         ),
       ),
@@ -41,19 +47,25 @@ class _UserPageState extends State<UserPage> {
   }
 
   void getUserDataFromGithub() async {
+    setState(() {
+      isLoading = true;
+    });
     final response = await http.get('https://api.github.com/users/$username');
     setState(() {
-      githubData = (response.statusCode == 200) ? GithubData.fromJson(jsonDecode(response.body)) : null;
+      isLoading = false;
+      githubData = (response.statusCode == 200)
+          ? GithubData.fromJson(jsonDecode(response.body))
+          : null;
     });
   }
 }
 
-class GithubData{
+class GithubData {
   final String bio;
 
-  GithubData({ this.bio });
+  GithubData({this.bio});
 
-  factory GithubData.fromJson(Map<String, dynamic> json){
+  factory GithubData.fromJson(Map<String, dynamic> json) {
     return GithubData(bio: json['bio']);
   }
 }
